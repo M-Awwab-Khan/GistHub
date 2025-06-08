463149e107be8a26c87e7da4c76a82ebfbc1f6ef
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-06-08 20:46:22 +0500

    feat: ai completions worked

--- app/api/code-completion/route.ts
+++ app/api/code-completion/route.ts
@@ -3,9 +3,38 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { CompletionCopilot, type CompletionRequestBody } from "monacopilot";
 
-const copilot = new CompletionCopilot(process.env.MISTRAL_API_KEY, {
-  provider: "mistral",
-  model: "codestral",
+const copilot = new CompletionCopilot(undefined, {
+  model: async (prompt) => {
+    console.log(
+      prompt.fileContent
+        .replace(/\*\*Current code:\*\*\n```\n|\n```$/g, "")
+        .replace("<|developer_cursor_is_here|>", "")
+    );
+    const response = await fetch(
+      "https://codestral.mistral.ai/v1/fim/completions",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.MISTRAL_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "codestral-latest",
+          prompt: prompt.fileContent
+            .replace(/\*\*Current code:\*\*\n```\n|\n```$/g, "")
+            .replace("<|developer_cursor_is_here|>", ""),
+          temperature: 0.2,
+          max_tokens: 256,
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      text: data.choices[0].message.content,
+    };
+  },
 });
 console.log(process.env.MISTRAL_API_KEY, "MISTRAL_API_KEY");
 
